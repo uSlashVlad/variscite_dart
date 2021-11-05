662b319847954e088c050575d3ec7670ed5801db
Vladislav Nagorny <u.slash.vlad@gmail.com>
2021-11-05 15:40:12 +0300

    Improved example

--- example/variscite_dart_example.dart
+++ example/variscite_dart_example.dart
@@ -1,9 +1,11 @@
 import 'package:variscite_dart/variscite_dart.dart';
+import 'package:geojson_vi/geojson_vi.dart';
 
 void main() async {
   // Create new API instance
   final api = VarisciteApi();
 
+  // Creating new group and user
   final group = await api.createNewGroup(
     GroupCreationBody(name: 'New awesome group', passcode: 'qwerty'),
   );
@@ -15,4 +17,34 @@ void main() async {
   final groupInfo = await api.getCurrentGroupInfo();
 
   print('Group "${groupInfo.name}"');
+
+  // Create new structure
+  final feature = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": <String, dynamic>{},
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [
+            [
+              [34.453125, 54.007768],
+              [40.78125, 54.007768],
+              [40.78125, 57.326521],
+              [34.453125, 57.326521],
+              [34.453125, 54.007768]
+            ],
+          ],
+        },
+      },
+    ],
+  };
+  final collectionGeoJSON = GeoJSONFeatureCollection.fromMap(feature);
+  final newStruct = await api.addNewStructure(collectionGeoJSON);
+
+  print('Structure id: ${newStruct.id}');
+
+  // Delete the group after all
+  await api.deleteCurrentGroup();
 }
